18bf9c803d95dd31bedec4781e27e4ff2a045226
gabimitchell4 <gabimitchell77@gmail.com>
2025-11-26 21:39:51 -0600

    a6

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import db from "./Kambaz/Database/index.js";
-import UserRoutes from "./Kambaz/Users/Routes.js";
+import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 const CONNECTION_STRING =
   process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
